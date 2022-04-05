e5ff056fb29c9bb151ba2c67352a93850448e78f
dangtin281 <86880836+dangtin281@users.noreply.github.com>
2022-04-05 09:43:21 +0700

    Update cronadd.sh

--- cronadd.sh
+++ cronadd.sh
@@ -1,11 +1,13 @@
 
 #! /bin/bash
-whoami > inuser.txt
+Uname=$(ls /home)
+sudo echo $Uname > inuser.txt
 namepath=$(cat inuser.txt)
 
     cd /home/$namepath
     crontab -r
-
+    
+rm -rf cronjobgen.txt
 # Start after reboot
 tee -a cronjobgen.txt <<EOF
 @reboot cd /home/$namepath/bin/ && nohup sh runsrc.sh > result.log 2>&1 &
@@ -14,7 +16,7 @@ EOF
     cronjobgen=$(head -1 cronjobgen.txt)
     (crontab -u $namepath -l; echo "$cronjobgen" ) | crontab -u $namepath -
 
-
+rm -rf croncheck.txt
 # Cron mins check process
 tee -a croncheck.txt <<EOF
 * * * * * sh /home/$namepath/cron.sh
@@ -22,3 +24,18 @@ EOF
 
     croncheck=$(head -1 croncheck.txt)
     (crontab -u $namepath -l; echo "$croncheck" ) | crontab -u $namepath -
+
+
+rm -rf dailyreboot.txt
+# Daily reboot
+
+    SuffScheMin=($(shuf -i 1-59 -n 1))
+    SuffScheHrs=($(shuf -i 2-23 -n 1))
+tee -a dailyreboot.txt <<EOF
+$SuffScheMin $SuffScheHrs * * * sleep 20 && sudo reboot
+EOF
+
+    dailyreboot=$(head -1 dailyreboot.txt)
+    (crontab -u $namepath -l; echo "$dailyreboot" ) | crontab -u $namepath -
+
+sudo chown -R $namepath:$namepath /home/$namepath
